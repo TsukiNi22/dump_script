688bb24c03b3b22faa5040af494aeef79533f198
TsukiNi22 <mat.d.ecole@gmail.com>
2025-12-19 09:56:38 +0100

    feat (dotfile): fix the fastfetch loading

--- Fedora/dotfile/fastfetch/rdm_img.sh
+++ Fedora/dotfile/fastfetch/rdm_img.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
 
-# Dossier des images
+# Dossiers
 IMG_DIR="$HOME/.config/fastfetch/img"
+BANANA_IMG="$HOME/.config/fastfetch/banana/banana.png"
 
-# Sélectionner un fichier image aléatoire
-RANDOM_IMG=$(find $IMG_DIR -type f | shuf -n 1)
+# Fichier de config
+CONFIG_SAVE="$HOME/.config/fastfetch/config_save.jsonc"
+CONFIG="$HOME/.config/fastfetch/config.jsonc"
 
-# Mettre à jour la configuration Fastfetch
-cp ~/.config/fastfetch/config_save.jsonc ~/.config/fastfetch/config.jsonc
-sed -i "s#IMG#$RANDOM_IMG#g" ~/.config/fastfetch/config.jsonc
+# Image par défaut : aléatoire
+RANDOM_IMG=$(find "$IMG_DIR" -type f | shuf -n 1)
+IMG="$RANDOM_IMG"
 
-# Exécuter Fastfetch
+# Gestion des arguments
+for arg in "$@"; do
+    case "$arg" in
+        -b|--banana)
+            IMG="$BANANA_IMG"
+            ;;
+    esac
+done
+
+# Vérification image
+if [[ ! -f "$IMG" ]]; then
+    echo "Erreur: can't found the given picture -> $IMG"
+    exit 1
+fi
+
+# Appliquer la config
+cp "$CONFIG_SAVE" "$CONFIG"
+sed -i "s#IMG#$IMG#g" "$CONFIG"
+
+# Lancer fastfetch
 fastfetch
